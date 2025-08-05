9df76432b383dfbd95ae9f28263818319e921f5a
kendulijhb024 <kendulijhb024@student.wethinkcode.co.za>
2025-08-05 12:00:31 +0200

    updated code

--- src/main/java/com/example/todoapp/entity/TodoEntity.java
+++ src/main/java/com/example/todoapp/entity/TodoEntity.java
@@ -1,4 +1,32 @@
 package com.example.todoapp.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Entity
+@Table(name = "tbl_todos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String title;
+
+    private Boolean completed;
+
+
+
+
 }
