fe62a489fb56b80031223252add6668a9c917062
kendulijhb024 <kendulijhb024@student.wethinkcode.co.za>
2025-08-05 15:35:06 +0200

    updated code

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -1,13 +1,24 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
+
 public class TodoController {
 
+    private final TodoRepository todoRepository;
+
     @GetMapping({"","/","/home"})
-    public String showHomePage(){
+    public String showHomePage(Model model){
+
+        model.addAttribute("todos", todoRepository.findAll());
+
+//        todoRepository.findAll();
         return "index";
     }
 }
--- src/main/java/com/example/todoapp/entity/TodoEntity.java
+++ src/main/java/com/example/todoapp/entity/TodoEntity.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @NoArgsConstructor
 @Builder
 
+
+
 public class TodoEntity {
 
     @Id
